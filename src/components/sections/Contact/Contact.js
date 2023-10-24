757c5136031771c6555a47cd8e7b78c2a4ed8d43
Nikita Staselovich <nikitastaselovich@gmail.com>
2023-10-24 09:15:33 +0300

    UPD: updated form

--- src/components/sections/Contact/Contact.js
+++ src/components/sections/Contact/Contact.js
@@ -1,14 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
 import Section from "../../../HOC/Section";
 
 export const Contact = () => {
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
   const openEmailClient = (event) => {
     event.preventDefault();
     window.open(
-      `https://mail.google.com/mail/?view=cm&fs=1&to=payotak@gmail.com&su="Alletanium LLC"&body=${message}&bcc=${email}`
+      `https://mail.google.com/mail/?view=cm&fs=1&to=payotak@gmail.com&su="Alletanium LLC"`
     );
   };
   return (
@@ -25,47 +23,13 @@ export const Contact = () => {
           </h6>
         </div>
         <div className="section-content">
-          <div className="row">
-            <div className="col-md-9 col-lg-7 mr-auto ml-auto">
-              <form>
-                <div className="form-group">
-                  <input
-                    id="name"
-                    type="text"
-                    className="form-control rounded-0"
-                    aria-describedby="emailHelp"
-                    placeholder="Enter Name..."
-                  />
-                </div>
-                <div className="form-group">
-                  <input
-                    onInput={(e) => setEmail(e.target.value)}
-                    id="email"
-                    type="email"
-                    className="form-control rounded-0"
-                    aria-describedby="emailHelp"
-                    placeholder="Enter email..."
-                  />
-                </div>
-                <div className="form-group">
-                  <textarea
-                    onInput={(e) => setMessage(e.target.value)}
-                    id="message"
-                    className="form-control rounded-0"
-                    rows="5"
-                    placeholder="Enter Message..."
-                  />
-                </div>
-                <div className="form-group text-center">
-                  <button
-                    onClick={openEmailClient}
-                    className="btn btn-block btn-primary rounded-0 mr-auto ml-auto"
-                  >
-                    Send
-                  </button>
-                </div>
-              </form>
-            </div>
+          <div className="d-flex justify-content-center ">
+            <button
+              onClick={openEmailClient}
+              className="btn btn-block btn-primary rounded-0 w-25"
+            >
+              Send
+            </button>
           </div>
         </div>
       </div>
